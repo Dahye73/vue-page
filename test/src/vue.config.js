097fdc1a8b22d1b2690daeb15f0560e3f84ab72e
Dahye73 <102746730+Dahye73@users.noreply.github.com>
2022-04-10 21:48:20 +0900

    echtest

--- test/src/vue.config.js
+++ test/src/vue.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = { 
   outputDir: './docs' ,
   publicPath: process.env.NODE_ENV = 'production' 
-  ?  '/miniproject/' 
+  ?  '/vue-page/' 
   : '/', 
   
 }
